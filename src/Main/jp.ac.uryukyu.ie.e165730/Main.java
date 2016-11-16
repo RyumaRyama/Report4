8122d663100a8ca920d728e495f352cd975e854c
Ryuma Maeshiro <e165730@ie.u-ryukyu.ac.jp>
2016-11-16 20:27:12 +0900

    Hero and Enemy after Extends LivingThing.

--- src/Main/jp.ac.uryukyu.ie.e165730/Main.java
+++ src/Main/jp.ac.uryukyu.ie.e165730/Main.java
@@ -3,13 +3,13 @@ package jp.ac.uryukyu.ie.e165730;
 
 public class Main {
     public static void main(String[] args){
-        Hero hero = new Hero("勇者", 10, 5);
+        Hero hero = new Hero("ヨシヒコ", 10, 5);
         Enemy enemy = new Enemy("スライム", 6, 3);
 
         System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
 
         int turn = 0;
-        while( hero.isDead() == false && enemy.isDead() == false ){
+        while(!hero.isDead() && !enemy.isDead()){
             turn++;
             System.out.printf("%dターン目開始！\n", turn);
             hero.attack(enemy);
